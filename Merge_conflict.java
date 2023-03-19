25104ff1a14668f13dcb2eaed740dd8e65ff8892
Alex Bril <brilsasha@gmail.com>
2023-03-19 19:33:38 +0200

    Merge_branch_2 commit

--- Merge_conflict.java
+++ Merge_conflict.java
@@ -1,5 +1,6 @@
 public class Merge_conflict {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I just wanna be ok!");
     }
 }
